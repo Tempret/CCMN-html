f64e336aad0f0871eb3e4f0f6340a6677067e1bd
Denis Khlopov <dkhlopov@e1r4p16.unit.ua>
2018-01-11 21:03:09 +0200

    add animate frames

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,8 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var inAnimation = 'fadeInUpBig';
+    var outAnimation = 'hinge';
+
+    $('.pop-up').each(function() {
+        $(this).addClass(outAnimation);
+    });
+
+    $('html').click(function (event) {
+        if (!$(event.target).hasClass('panel-item')) {
+            $('.pop-up').each(function() {
+                $(this).removeClass(inAnimation);
+                $(this).addClass(outAnimation);
+            });
+        };
+    });
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
-            top = $(id).offset().top - 70;
+            top = $(id).offset().top - 110;
         $('body,html').animate({scrollTop: top}, 500);
+    });
+    $('.panel-item').click(function() {
+        var popUp = $(this).find('.pop-up');
+
+        if (popUp.css("display") == 'none') {
+            popUp.css('display', 'block');
+        };
+
+        if (popUp.hasClass(outAnimation)) {
+            $('.pop-up').each(function() {
+                $(this).removeClass(inAnimation);
+                $(this).addClass(outAnimation);
+            });
+            popUp.addClass(inAnimation);
+            popUp.removeClass(outAnimation);
+        }
+        else if (popUp.hasClass(inAnimation)) {
+            $('.pop-up').each(function() {
+                $(this).removeClass(inAnimation);
+                $(this).addClass(outAnimation);
+            });
+        };
     });
 });
