e966800b34bff2c811963a69b4822dcbca63335f
Denis Khlopov <dkhlopov@e1r4p16.unit.ua>
2018-01-23 12:09:20 +0200

    material design

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,45 +1,57 @@
 $(document).ready(function() {
-    var inAnimation = 'fadeInUpBig';
-    var outAnimation = 'hinge';
+    var inAnimation = 'bounceIn';
+    var outAnimation = 'bounceOut';
 
     $('.pop-up').each(function() {
         $(this).addClass(outAnimation);
     });
 
-    $('html').click(function (event) {
-        if (!$(event.target).hasClass('panel-item')) {
-            $('.pop-up').each(function() {
-                $(this).removeClass(inAnimation);
-                $(this).addClass(outAnimation);
-            });
-        };
+    $('#submit').click(function () {
+        var username = "RO";
+        var password = "just4reading";
+        $.ajax({
+            url: "https://cisco-cmx.unit.ua/api/location/v1/attributes",
+            type: "GET",
+            jsonp: 'jsonp_callback',
+            dataType: "json",
+            headers: {
+                "Authorization": "Basic " + btoa(username + ":" + password)
+            },
+            success: function (data) {
+                console.log(data);
+            },
+        });
     });
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top - 110;
-        $('body,html').animate({scrollTop: top}, 500);
-    });
-    $('.panel-item').click(function() {
-        var popUp = $(this).find('.pop-up');
 
-        if (popUp.css("display") == 'none') {
-            popUp.css('display', 'block');
-        };
+    // $('html').click(function (event) {
+    //     if (!$(event.target).hasClass('panel-item')) {
+    //         $('.pop-up').each(function() {
+    //             $(this).removeClass(inAnimation);
+    //             $(this).addClass(outAnimation);
+    //         });
+    //     };
+    // });
 
-        if (popUp.hasClass(outAnimation)) {
-            $('.pop-up').each(function() {
-                $(this).removeClass(inAnimation);
-                $(this).addClass(outAnimation);
-            });
-            popUp.addClass(inAnimation);
-            popUp.removeClass(outAnimation);
-        }
-        else if (popUp.hasClass(inAnimation)) {
-            $('.pop-up').each(function() {
-                $(this).removeClass(inAnimation);
-                $(this).addClass(outAnimation);
-            });
-        };
-    });
+    // $('.panel-item').click(function() {
+    //     var popUp = $(this).find('.pop-up');
+    //
+    //     if (popUp.css("display") == 'none') {
+    //         popUp.css('display', 'block');
+    //     };
+    //
+    //     if (popUp.hasClass(outAnimation)) {
+    //         $('.pop-up').each(function() {
+    //             $(this).removeClass(inAnimation);
+    //             $(this).addClass(outAnimation);
+    //         });
+    //         popUp.addClass(inAnimation);
+    //         popUp.removeClass(outAnimation);
+    //     }
+    //     else if (popUp.hasClass(inAnimation)) {
+    //         $('.pop-up').each(function() {
+    //             $(this).removeClass(inAnimation);
+    //             $(this).addClass(outAnimation);
+    //         });
+    //     };
+    // });
 });
