8d17e388b62490ac0494ded6e8d65523be6abda0
tempret <tempret@gmail.com>
2018-01-24 01:13:43 +0200

    add animation to dashboards

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var ccmn = {
                 "Authorization": "Basic " + btoa(username + ":" + password)
             },
             success: function (data) {
-                var response=transport.responseText.evalJSON(true);
+                var response = transport.responseText.evalJSON(true);
             }
         });
 
@@ -25,13 +25,44 @@ var ccmn = {
     }
 };
 
-$(document).ready(function() {
-    ccmn.inAnimation = 'bounceIn';
-    ccmn.outAnimation = 'bounceOut';
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback();
+            }
+        });
+        return this;
+    }
 });
+
+$(document).ready(function() {
+    inAnimation = 'fadeInRightBig';
+
+    $('.panel-item').click( function (event) {
+        var cur = event.target;
+        var targets = $('.panel-item');
+        var cur_index = 0;
+
+        for (; cur_index < targets.length; cur_index++)
+            if (cur === targets[cur_index])
+                break;
+
+        $.each($('.dashboard-item'), function (index) {
+            $(this).addClass('hide');
+            $(this).removeClass('animated visible ' + inAnimation);
+        });
+        $($('.dashboard-item')[cur_index]).removeClass('hide');
+        $($('.dashboard-item')[cur_index]).addClass('animated visible ' + inAnimation);
+    });
+});
+
+
 
 //  "https://cisco-cmx.unit.ua/api/location/v1/attributes",
 
-var siteId = ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET');
-
-console.log(siteId);
+// var siteId = ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET');
+//
+// console.log(siteId);
