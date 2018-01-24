3f318c2ef2e8e845e2ceed89f8a0824a4098da48
Denis Khlopov <dkhlopov@e1r4p16.unit.ua>
2018-01-24 21:11:36 +0200

    need to fix click

--- js/main.js
+++ js/main.js
@@ -2,49 +2,46 @@ var ccmn = {
 
     apis : ['locate', 'presence'],
 
-    makeApiRequest : function (url, api, type) {
+    siteId : NaN,
+
+    setSiteId : function (data) {
+        ccmn.siteId = data[0].aesUId;
+    },
+
+    setTotalVisitors : function (data) {
+        // console.log(data);
+        $('.total-visitors').next().text(data);
+    },
+
+    makeApiRequest : function (url, api, type, callback, data) {
         var username = $('#login').val();
         var password = $('#' + api).val();
 
-        console.log(username + ':' + password);
-
-        var response = $.ajax({
+        $.ajax({
             url: 'https://' + url,
-            type: "GET",
+            type: type,
             jsonp: 'jsonp_callback',
+            data: data,
             dataType: "json",
             headers: {
                 "Authorization": "Basic " + btoa(username + ":" + password)
             },
             success: function (data) {
-                var response = transport.responseText.evalJSON(true);
+                callback(data);
             }
         });
-
-        return response;
     }
 };
-
-$.fn.extend({
-    animateCss: function (animationName, callback) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-            if (callback) {
-                callback();
-            }
-        });
-        return this;
-    }
-});
 
 $(document).ready(function() {
     inAnimation = 'fadeInRightBig';
 
-    $('.panel-item').click( function (event) {
+    $('.panel-item, .menu-icons').on("click", function (event) {
         var cur = event.target;
         var targets = $('.panel-item');
         var cur_index = 0;
+
+
 
         for (; cur_index < targets.length; cur_index++)
             if (cur === targets[cur_index])
@@ -56,13 +53,14 @@ $(document).ready(function() {
         });
         $($('.dashboard-item')[cur_index]).removeClass('hide');
         $($('.dashboard-item')[cur_index]).addClass('animated visible ' + inAnimation);
+
+        console.log($(cur).hasClass('hide'));
     });
+
+    ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET', ccmn.setSiteId, NaN);
+
+    var timerId = setInterval(function() {
+        ccmn.makeApiRequest('cisco-presence.unit.ua/api/presence/v1/connected/count/today', ccmn.apis[1], 'GET', ccmn.setTotalVisitors, {siteId:ccmn.siteId});
+    }, 5000);
+
 });
-
-
-
-//  "https://cisco-cmx.unit.ua/api/location/v1/attributes",
-
-// var siteId = ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET');
-//
-// console.log(siteId);
