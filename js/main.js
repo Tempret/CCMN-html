60e0e1227b9615b43d604fa8fd804c86e1e879a2
Denis Khlopov <dkhlopov@e1r4p16.unit.ua>
2018-01-25 21:16:50 +0200

    datepicker and chart.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var ccmn = {
 
     setSiteId : function (data) {
         ccmn.siteId = data[0].aesUId;
+        console.log("Site id:", data[0].aesUId);
     },
 
     setTotalVisitors : function (data) {
@@ -30,18 +31,29 @@ var ccmn = {
                 callback(data);
             }
         });
-    }
+    },
+
+    randomArray : function (length, max) {
+        return Array.apply(null, Array(length)).map(function() {
+            return Math.round(Math.random() * max);
+        });
+    },
+
+    chart: NaN,
+
 };
 
 $(document).ready(function() {
     inAnimation = 'fadeInRightBig';
 
-    $('.panel-item, .menu-icons').on("click", function (event) {
+    /* DASHBOARD ANIMATION */
+    $('.panel-item').on("click", function (event) {
         var cur = event.target;
         var targets = $('.panel-item');
         var cur_index = 0;
 
-
+        if (!$(cur).hasClass('panel-item'))
+            cur = cur.parentElement;
 
         for (; cur_index < targets.length; cur_index++)
             if (cur === targets[cur_index])
@@ -53,9 +65,9 @@ $(document).ready(function() {
         });
         $($('.dashboard-item')[cur_index]).removeClass('hide');
         $($('.dashboard-item')[cur_index]).addClass('animated visible ' + inAnimation);
-
-        console.log($(cur).hasClass('hide'));
     });
+    /* dashboard animation END*/
+
 
     ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET', ccmn.setSiteId, NaN);
 
@@ -63,4 +75,68 @@ $(document).ready(function() {
         ccmn.makeApiRequest('cisco-presence.unit.ua/api/presence/v1/connected/count/today', ccmn.apis[1], 'GET', ccmn.setTotalVisitors, {siteId:ccmn.siteId});
     }, 5000);
 
+    /* charts END*/
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Yellow"],
+            datasets: [{
+                label: '# of Votes',
+                data: [46, 28, 9, 63, 9, 1, 37, 75, 32, 89, 74, 2, 54, 70, 69, 23, 57, 47, 72, 55],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: '# of Votes',
+                data: [46, 28, 9, 63, 9, 1, 37, 75, 32, 89, 74, 2, 54, 70, 69, 23, 57, 47, 72, 55],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    ccmn.chart = myChart;
+
 });
+
+
