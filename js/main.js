01f15727135f9058f041b3fde9effb34a7fd14b4
Denis Khlopov <dkhlopov@e2r6p7.unit.ua>
2018-01-26 19:26:09 +0200

    fucking js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var ccmn = {
 
     apis : ['locate', 'presence'],
 
+    table_data_total : {
+        'connected': null,
+        'passerby': null,
+        'visitors': null,
+        'peak': null
+    },
+
     siteId : NaN,
 
     setSiteId : function (data) {
@@ -14,7 +21,7 @@ var ccmn = {
         $('.total-visitors').next().text(data);
     },
 
-    makeApiRequest : function (url, api, type, callback, data) {
+    makeApiRequest : function (url, api, type, callback, args) {
         var username = $('#login').val();
         var password = $('#' + api).val();
 
@@ -22,13 +29,13 @@ var ccmn = {
             url: 'https://' + url,
             type: type,
             jsonp: 'jsonp_callback',
-            data: data,
+            data: args,
             dataType: "json",
             headers: {
                 "Authorization": "Basic " + btoa(username + ":" + password)
             },
             success: function (data) {
-                callback(data);
+                callback(data, arguments[1]);
             }
         });
     },
@@ -39,14 +46,37 @@ var ccmn = {
         });
     },
 
+    getTimeArray : function() {
+        var arr = [];
+        var item;
+
+        for(var i = 0; i <= 24; i++) {
+            if (i < 10)
+                item = '0' + i + '.00';
+            else
+                item = i + '.00';
+            arr.push(item);
+        };
+        return arr;
+    },
+
+    setTableTotalVisitors: function(data, cellId) {
+        $(cellId).text(data[0]);
+    },
+
     chart: NaN,
+
+
 
 };
 
 $(document).ready(function() {
     inAnimation = 'fadeInRightBig';
+    ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET', ccmn.setSiteId, NaN);
 
-    /* DASHBOARD ANIMATION */
+    ccmn.makeApiRequest('');
+
+    /* DASHBOARDS ANIMATION */
     $('.panel-item').on("click", function (event) {
         var cur = event.target;
         var targets = $('.panel-item');
@@ -66,10 +96,22 @@ $(document).ready(function() {
         $($('.dashboard-item')[cur_index]).removeClass('hide');
         $($('.dashboard-item')[cur_index]).addClass('animated visible ' + inAnimation);
     });
-    /* dashboard animation END*/
+    /* dashboards animation END*/
+
+    /* DATE SELECT CHANGE EVENT */
+    $('#date-selection').change(function () {
+        var value = $(this).val();
+
+        if (value == 'custom')                          /*  Hide custom datepicher if custom select is active */
+            $('#custom-date').removeClass('hide');      /* | */
+        else                                            /* | */
+            $('#custom-date').addClass('hide');         /*---*/
+
+    });
+    /* date select change event END */
 
 
-    ccmn.makeApiRequest('cisco-presence.unit.ua/api/config/v1/sites', ccmn.apis[1], 'GET', ccmn.setSiteId, NaN);
+
 
     var timerId = setInterval(function() {
         ccmn.makeApiRequest('cisco-presence.unit.ua/api/presence/v1/connected/count/today', ccmn.apis[1], 'GET', ccmn.setTotalVisitors, {siteId:ccmn.siteId});
@@ -80,25 +122,41 @@ $(document).ready(function() {
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Yellow"],
+            labels: ccmn.getTimeArray(),
             datasets: [{
                 label: '# of Votes',
                 data: [46, 28, 9, 63, 9, 1, 37, 75, 32, 89, 74, 2, 54, 70, 69, 23, 57, 47, 72, 55],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 99, 132, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255,99,132,1)'
                 ],
                 borderWidth: 1
             },
@@ -106,20 +164,43 @@ $(document).ready(function() {
                 label: '# of Votes',
                 data: [46, 28, 9, 63, 9, 1, 37, 75, 32, 89, 74, 2, 54, 70, 69, 23, 57, 47, 72, 55],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255,99,132,1)',
+
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+
+
                 ],
                 borderWidth: 1
             }
@@ -136,7 +217,24 @@ $(document).ready(function() {
         }
     });
     ccmn.chart = myChart;
-
 });
+
+
+// backgroundColor: [
+//     'rgba(255, 99, 132, 0.2)',
+//     'rgba(54, 162, 235, 0.2)',
+//     'rgba(255, 206, 86, 0.2)',
+//     'rgba(75, 192, 192, 0.2)',
+//     'rgba(153, 102, 255, 0.2)',
+//     'rgba(255, 159, 64, 0.2)'
+// ],
+//     borderColor: [
+//     'rgba(255,99,132,1)',
+//     'rgba(54, 162, 235, 1)',
+//     'rgba(255, 206, 86, 1)',
+//     'rgba(75, 192, 192, 1)',
+//     'rgba(153, 102, 255, 1)',
+//     'rgba(255, 159, 64, 1)'
+// ],
 
 
